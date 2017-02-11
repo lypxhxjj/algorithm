029c89ee2697e8f949bf80ef830169f835cd77ae
lypxhxjj <792512795@qq.com>
2017-02-12 01:42:38 +0800

    Update code.cpp
    排序算法

--- code.cpp
+++ code.cpp
@@ -1,57 +1,49 @@
-基础砖块
+/* 
+*  此文件内收集优美的解法，主要是根据C++11新特性来实现的一些常见功能；
+*/
 
-如同建筑工人需要用到砖块来构建大楼，我们也需要用到一些基础的函数。幸而 STL 提供了很多这样的函数。
+//接下来几个是排序算法，大部分都可以使用几行代码就可以完成；
+//引用参见：https://zhuanlan.zhihu.com/p/20652569?refer=xiaochi
 
-#include <algorithm>    // min_element, iter_swap, 
-                        // upper_bound, rotate, 
-                        // partition, 
-                        // inplace_merge,
-                        // make_heap, sort_heap, push_heap, pop_heap,
-                        // is_heap, is_sorted
-#include <cassert>      // assert 
-#include <functional>   // less
-#include <iterator>     // distance, begin, end, next
-其中 std::begin() / std::end() / std::next() / std::is_sorted / std::is_heap 这些算法都是自 C++11 才有的。
-
-选择排序
-
-选择排序的时间复杂度总是，不管数据是何种样子的。但它有一点好，就是总体的交换次数最少。所以，如果交换元素的开销很大，不妨用这个算法。
-
-这个算法的实现方法很简单，重复使用 std::min_element 来发现最小的元素，然后用 iter_swap 将它交换到应在的位置。
-
+/* 选择排序算法：
+* （1）找到最小值位置可以使用std::min_element函数；
+* （2）实现算法使用迭代器更好用，此时交换元素，使用std::iter_swap函数
+*/
 template<class FwdIt, class Compare = std::less<>>
 void selection_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 {
     for (auto it = first; it != last; ++it) {
         auto const selection = std::min_element(it, last, cmp);
         std::iter_swap(selection, it); 
-        assert(std::is_sorted(first, std::next(it), cmp));
+        //assert(std::is_sorted(first, std::next(it), cmp));
     }
 }
-注意 selection_sort 在每次循环开启之前， [first, it) 里的元素就已经排序好了。和 std::sort比起来，这个迭代器只要求是前向迭代器，而非像 std::sort() 一样需要随机迭代器。
 
-插入排序
 
-虽然只是最基本的排序方法，虽然最坏的情况要达到，但是插入排序依然是很有用的排序方法。因为它是适应性的，如果数据基本有序，就有很好的效果。或者数据的量很小，它也会有很小的表现（因为它的开销很小）。因这个原因，插入排序通常会作为其他「大」递归算法（如快速排序和合并排序）的基本例。
-
-为了实现 insertion_sort ，我们不停的使用 std::upper_bound 来找到当前元素应该在的位置，然后使用 std::rotate 将当前元素放到正确的位置。
-
+/* 插入排序算法：
+* （1）找到插入位置使用：upper_bound函数；
+* （2）找到插入位置，插入的话可以使用旋转操作：std::rotate;
+*     ----》std::rotate也可以理解为交换两部分迭代器中的内容，参数分别是first,middle,last；
+*     ----》迭代器实现加减法运算需要使用：std::next(Iter, n)；来实现；
+*/
 template<class FwdIt, class Compare = std::less<>>
 void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 {
     for (auto it = first; it != last; ++it) {
         auto const insertion = std::upper_bound(first, it, *it, cmp);
         std::rotate(insertion, it, std::next(it)); 
-        assert(std::is_sorted(first, std::next(it), cmp));
+        //assert(std::is_sorted(first, std::next(it), cmp));
     }
 }
-注意，insertion_sort 的循环不变量是已经将 [first, it) 排序。插入排序也是只需要前向迭代器。
-快速排序
 
-快速排序是一个很优秀的排序算法。如果不要求稳定排序，一般我们会选择这个算法。它的期望复杂度是，最坏复杂度是。
 
-即使是最简单的快速排序，也是很复杂的。下面的算法先找到中间位置的元素，作为pivot，然后通过两次 std::partition ( O(N) 的复杂度) to调用，来找到比pivot小的，比pivot大的。最后，这两部分再次递归的调用快速排序。
-
+/* 快速排序算法：
+* （1）一般使用迭代器来实现算法，更具有通用性，涉及到的数组大小使用std::distance，迭代器的加减操作：std::next和std::prev；
+* （2）使用两个std::partition函数，第一个是找到小于*it的数组，第二个是找到大于等于*it中等于的部分，最后递归的只需要是不等于的那个部分；
+*     ----》第三个参数可以使用仿函数来实现，std::bind1st(std::greater<int>(), *it)代表*it大的话放左边，op(*it, value)，*it是第一个参数；
+*     ----》等于的话：std::bind1st(std::equal_to<int>)
+*     ----》std::greater<int>()，模板参数中的int，替换掉的话，可以使用Iter::value_type.(traits技法)
+*/
 template<class FwdIt, class Compare = std::less<>>
 void quick_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 {
@@ -67,7 +59,6 @@ void quick_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
     quick_sort(first, middle1, cmp); // assert(std::is_sorted(first, middle1, cmp));
     quick_sort(middle2, last, cmp);  // assert(std::is_sorted(middle2, last, cmp));
 }
-只有pivot将数据分为大致相等的两部分，才能达到的复杂度。但的复杂度并不能将这种pivot选出来。因此大家想了很多办法，欲知详情，请去看原答案。
 
 归并排序
 
