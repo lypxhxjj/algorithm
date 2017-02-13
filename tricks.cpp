37b402b5170ebc22e2f73b22c8c5fa282040b965
lypxhxjj <792512795@qq.com>
2017-02-14 03:16:21 +0800

    Update tricks.cpp
    修改了例子

--- tricks.cpp
+++ tricks.cpp
@@ -76,11 +76,13 @@ void quick_sort3(Iter it1, Iter it2) {
 }
 
 /*技巧3：代码的缩减技巧总结
+* 较一般性：
 *（1）边界检查，可以先看看是否包括在循环中了，此题就不需要最开始的边界检查；（可以最后检查）
 *（2）同一作用的变量考虑在同一行定义，定义dummy和result可以在同一行：ListNode dummy, *result = &dummy;
 * (3)循环之外解决特殊情况，可以考虑将其放到循环的判定条件中；while (l1 || l2 || flag)
-*（4）两个list不知道哪个到头没，可以分开放到临时变量中，其实也是不用的； int val = flag + l1 ? l1->next : 0;
 *（5）能不使用临时变量就不使用；val = val % 10可以合并到下一行；
+* 特殊性：
+*（4）两个list不知道哪个到头没，可以分开放到临时变量中，其实也是不用的； int val = flag + l1 ? l1->next : 0;
 *（6）其实进位的flag既可以当做进位，又可以当做总和；
 */
 //化简之前的代码：
@@ -107,29 +109,17 @@ void quick_sort3(Iter it1, Iter it2) {
     }
     
 //化简之后的代码
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode preHead(0), *p = &preHead;
-    int extra = 0;
-    while (l1 || l2 || extra) {
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
-        extra = sum / 10;
-        p->next = new ListNode(sum % 10);
-        p = p->next;
-        l1 = l1 ? l1->next : l1;
-        l2 = l2 ? l2->next : l2;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0), *result = &dummy;
+        int flag = 0;
+        while (l1 || l2 || flag) {
+            flag += (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            result->next = new ListNode(flag % 10);
+            flag /= 10;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+            result = result->next;
+        } 
+        return dummy.next;
+        
     }
-    return preHead.next;
-}
-//或者
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode preHead(0), *p = &preHead;
-    int extra = 0;
-    while (l1 || l2 || extra) {
-        if (l1) extra += l1->val, l1 = l1->next;
-        if (l2) extra += l2->val, l2 = l2->next;
-        p->next = new ListNode(extra % 10);
-        extra /= 10;
-        p = p->next;
-    }
-    return preHead.next;
-}
