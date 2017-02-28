0c06ed7c9b862dc6a0cbc476c98b8ae718f55bf5
lypxhxjj <792512795@qq.com>
2017-02-28 16:20:11 +0800

    Update tree.cpp
    二叉搜索树交换错误的节点的思路是，中序遍历找到两个错误节点，第一个错误节点在前继位置，第二个在后继位置。两个节点的保存一个赋值一遍，一个赋值两遍，就可以了；

--- tree.cpp
+++ tree.cpp
@@ -1,6 +1,8 @@
-/*1  判断一颗树是否是二叉搜索树：
+/*
+1  判断一颗树是否是二叉搜索树：
+（1）与二叉搜索树有关的，很可能是中序遍历；
 */
-//思路1：中序遍历的过程中进行判断；
+
 //错误：需要使用LONG_MIN;
 class Solution {
     void helper(bool& result, TreeNode* root, long long& value) {
@@ -37,5 +39,60 @@ public:
             root = root->right;
         }
         return true;
+    }
+};
+
+/*
+2  二叉搜索树有两个节点颠倒了，交换过来
+（1）与二叉搜索树相关的使用中序遍历；
+（1）思路是难点：两个节点第一次不顺序时，需要被交换的是前继节点；第二次不顺序时，需要被交换的是后继节点；没有第二次的话，说明只有两个节点，就交换那两个节点就好了；
+（2）保存前继节点pre，只有在中序遍历的时候保存前继节点才有意义；
+*/
+
+//使用前继节点，需要四个节点，遍历过程中的前继和后继，保存起来的前继和后继；
+class Solution {
+    void inorder(TreeNode* root, TreeNode*& value, TreeNode*& pre, TreeNode*& cur) {
+        if (!root) return;
+        inorder(root->left, value, pre, cur);
+        if (root->val <= value->val) {
+            if (!pre) pre = value;          //这个肯定有，所以只赋值一次；
+            cur = root;                     //这个不一定有，所以赋值两次；
+        }
+        else
+            value = root;
+        inorder(root->right, value, pre, cur);
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* value = new TreeNode(INT_MIN);
+        TreeNode* tmp = nullptr;
+        TreeNode* tmp1 = nullptr;
+        inorder(root, value, tmp, tmp1);
+        swap(tmp->val, tmp1->val);
+    }
+};
+
+//对应的非递归算法，完全是上面的拷贝
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* pre = new TreeNode(INT_MIN);
+        TreeNode* pre1 = nullptr, *cur = nullptr;
+        stack<TreeNode*> s;
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            if (root->val < pre->val) {
+                if (!pre1) pre1 = pre;
+                cur = root;
+            }
+            pre = root;
+            s.pop();
+            root = root->right;
+        }
+        swap(pre1->val, cur->val);
     }
 };
