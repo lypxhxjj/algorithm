e84894c080befb1ab1180e1c3bfcfe780096ca41
lypxhxjj <792512795@qq.com>
2017-03-07 01:11:23 +0800

    Update and rename dynamic programming to dynamic programming.cpp
    添加了使用额外一行来保存变量的结构

--- dynamic programming.cpp
+++ dynamic programming.cpp
@@ -28,3 +28,17 @@ public:
         return triangle[0][0];
     }
 };
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        int n = triangle.size();
+        vector<int> dp(triangle.back());
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
+            }
+        }
+        return dp[0];
+    }
+};
