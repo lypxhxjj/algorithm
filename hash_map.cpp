4478bd7e7932b76a46637f0d8c61b4a7f34179a3
lypxhxjj <792512795@qq.com>
2017-03-07 21:29:27 +0800

    Create hash_map.cpp
    hashmap去解决与索引有关的题，可以使用insert和erase搭配解题，可以搭配指针解题，此时注意指针的含义；

---
+++ hash_map.cpp
@@ -0,0 +1,50 @@
+//返回与索引有关的问题，一般使用hash_map，此时无法使用排序；
+//1  找到数组中加和为target的两个数的下标值；
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i) {
+            int tmp = target - nums[i];
+            if (map.find (tmp) != map.end())
+                return {map[tmp], i};         //返回只有两个元素的vector，就可以直接这么写；
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+//可以考虑unorder_set的insert和erase搭配来解题：
+//2  字符串中最长无重复子串的长度：
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int result = 0, left = 0;
+        for (int i = 0; i < s.size(); ) {             //不是每次都i++;
+            if (set.find(s[i]) == set.end()) {
+                set.insert(s[i++]);
+                result = max(result, (int)set.size());    //有时候result每次一更新，是不错的方法；
+            }
+            else set.erase(s[left++]);                //insert和erase搭配，好用的方法；
+        }
+        return result;
+    }
+};
+
+//可以考虑使用指针来搭配解题，指针的左右是标记左边合理的位置
+//题目同2
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int res = 0, left = 0;
+        unordered_map<char, int> map;                             //map中元素也很重要，代表的是最后一次出现的位置；
+        for (int i = 0; i < s.size(); ++i) {
+            if (map.find(s[i]) != map.end() && left <= map[s[i]]) //指针的含义很重要，代表目前为止前边不重复字符的第一个；
+                left = map[s[i]] + 1;
+            map[s[i]] = i;
+            res = max(res, i - left + 1);                         //值的更新与left和i有关；
+        }
+        return res;
+    }
+};
