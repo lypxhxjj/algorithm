203a93da4d738147e2e8023cce044684c16fbecb
lypxhxjj <792512795@qq.com>
2017-03-16 19:01:39 +0800

    Update binary_search.cpp
    使用long long 来表示和

--- binary_search.cpp
+++ binary_search.cpp
@@ -32,3 +32,19 @@ public:
         return false;
     }
 };
+
+//使用long long 的思想，任何一个和都可能越界；
+//有序数组和为target的索引；
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while (i < j) {
+            long long add = numbers[i] + numbers[j];
+            if (add == target) return {i + 1, j + 1};
+            else if (add > target) --j;
+            else ++i;
+        }
+        return {};
+    }
+};
