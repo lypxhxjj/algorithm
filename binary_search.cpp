c746b277281a3b64a302835bfc463caea215a9c8
lypxhxjj <792512795@qq.com>
2017-03-16 18:58:58 +0800

    Update binary_search.cpp
    合并一下

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,7 +6,7 @@ public:
     int findMin(vector<int>& arr) {
         if (arr.size() == 0) return 0;
         auto begin = arr.begin(), end = arr.end() - 1;
-        while (begin < end) {
+        while (begin < end) {                               //找一个数的话，那就不能相等，最后返回值是相等；
             auto mid = begin + (end - begin) / 2;
             int tmp = *end;
             if (*mid > tmp) begin = mid + 1;
@@ -14,5 +14,21 @@ public:
             else end = mid;
         }
         return *begin;
+    }
+};
+
+//判断一个数是否是完全平方数；
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int begin = 1, end = num;                       //begin和end代表的含义是第一个可能解和最后一个可能解；
+        while (begin <= end) {
+            long long mid = begin + (end - begin) / 2;  //long long的使用；
+            long long mul = mid * mid;
+            if (mul == num) return true;                //返回值是bool好办，直接在满足条件的地方返回true就好了；
+            else if (mul > num) end = mid - 1;
+            else begin = mid + 1;
+        }
+        return false;
     }
 };
